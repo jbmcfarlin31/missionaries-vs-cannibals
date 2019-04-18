ddff8091c64b1cd7587397f17a3f86f25487aff7
jbmcfarlin31 <38010699+jbmcfarlin31@users.noreply.github.com>
2019-04-18 09:57:12 -0600

    Update GameLogic.py

--- GameLogic.py
+++ GameLogic.py
@@ -5,13 +5,12 @@ class GameLogic(object):
         self.prompt_choices = {
         "m": "missionary",
         "c": "cannibal",
+        "n": "no",
         "q": "quit"
         }
 
         self.response = ""
         self.response2 = ""
-        self.response3 = ""
-
 
     def display_locations(self, east_bank, west_bank, canoe):
         """ Handles the logic for displaying the locations """
@@ -60,12 +59,13 @@ class GameLogic(object):
                     print(canoe)
 
                 elif canoe['location'] == "west":
-                    if i == "m":
-                        east_bank['missionaries'] = east_bank['missionaries'] + 1
-                        west_bank['missionaries'] = west_bank['missionaries'] - 1
-                    else:
-                        east_bank['cannibals'] = east_bank['cannibals'] + 1
-                        west_bank['cannibals'] = west_bank['cannibals'] - 1
+                    if i != "n":
+                        if i == "m":
+                            east_bank['missionaries'] = east_bank['missionaries'] + 1
+                            west_bank['missionaries'] = west_bank['missionaries'] - 1
+                        else:
+                            east_bank['cannibals'] = east_bank['cannibals'] + 1
+                            west_bank['cannibals'] = west_bank['cannibals'] - 1
 
                     print("Variables west manipulation$$$$$$")
                     print(user_choice)
@@ -104,111 +104,118 @@ class GameLogic(object):
         """ Handles the logic needed for asking a user for information """
         global response
         global response2
-        global response3
 
         response = ""
         response2 = ""
-        response3 = ""
-
-        #print("BEGIN: Response3 = ", response3)
 
         if canoe['location'] == "east":
-            response = input("Who would you like to send to the west bank? Type 'm' for missionary or 'c' for cannibal or 'q' to quit:")
+            response = input("Who would you like to send to the west bank? Type 'm' for missionary or 'c' for cannibal: ")
             response = str(response)
 
-            response2 = input("Who would you like to send next to the west bank? Type 'm' for missionary or 'c' for cannibal or 'q' to quit:")
+            if response == "q":
+                return (response)
+
+            response2 = input("Who would you like to send next to the west bank? Type 'm' for missionary or 'c' for cannibal or 'q' to quit: ")
             response2 = str(response2)
 
-            try:
-                value = self.prompt_choices[response]
-                value2 = self.prompt_choices[response2]
-            except Exception as error:
-                print("That is not a valid choice. Please try again.")
-                self.user_prompt(east_bank, west_bank, canoe)
-
-            if value != "quit" and value2 != "quit":
-                validate_move = self.ValidateMove(response, east_bank, west_bank, canoe, response2)
-                if validate_move is True:
-                    print("\nSending a {} and a {}... good luck!".format(value, value2))
-                    return (response, response2, response3)
-                else:
-                    print("Your selection is invalid - move will not work.")
-                    self.user_prompt(east_bank, west_bank, canoe)
-            else:
-                print("Thank you for playing!")
-
         if canoe['location'] == "west":
-            response3 = input("Who would you like to take the boat back to the east bank? Type 'm' for missionary or 'c' for cannibal or 'q' to quit:")
-            response3 = str(response3)
-            #print("Response3 = ", response3)
+            response = input("Who would you like to take the boat back to the east bank? Type 'm' for missionary or 'c' for cannibal: ")
+            response = str(response)
 
-            try:
-                value = self.prompt_choices[response3]
-            except Exception as error:
-                print("That is not a valid choice. Please try again.")
-                self.user_prompt(east_bank, west_bank, canoe)
+            if response == "q":
+                return (response)
+            
+            response2 = input("Who would you like to send another person back to the east bank? Type 'm' for missionary, 'c' for cannibal, 'n' for no, or 'q' to quit: ")
+            response2 = str(response2)
 
-            if value != "quit":
-                validate_move = self.ValidateMove(response3, east_bank, west_bank, canoe, None)
-                if validate_move is True:
-                    #print(response, response2, response3)
+        try:
+            value = self.prompt_choices[response]
+            value2 = self.prompt_choices[response2]
+        except Exception as error:
+            print("That is not a valid choice. Please try again.")
+            self.user_prompt(east_bank, west_bank, canoe)
 
-                    print("\nReturning a {}... good luck!".format(value))
+        if value != "quit" and value2 != "quit" and value != "no":
+
+            validate_move = self.ValidateMove(response, east_bank, west_bank, canoe, response2)
+            if validate_move is True:
+                if value2 == "no":
+                    print("\nSending a {}... good luck!".format(value))
                 else:
-                    print("Your selection is invalid - move will not work.")
-                    #print(response, response2, response3)
+                    print("\nSending a {} and a {}... good luck!".format(value, value2))
 
-                    self.user_prompt(east_bank, west_bank, canoe)
+                return (response, response2)
             else:
-                print("Thank you for playing!")
+                print("Your selection is invalid - move will not work.")
+                self.user_prompt(east_bank, west_bank, canoe)
+        elif value == "no":
+            print("Your selection is invalid - move will not work.")
+            self.user_prompt(east_bank, west_bank, canoe)
+        else:
+            response, response2 = "q", "q"
 
-            #print("END: Response3 = ", response3)
+        return (response, response2)
 
-        return (response, response2, response3)
+    def _ValidateMove(self, check_dict, user_choice, user_choice2):
+        """ Handles repetitive use of logic for validating the move """
+
+        # If the user chooses both missionaries to send...
+        if user_choice == 'm' and user_choice2 == "m":
+            if check_dict['missionaries'] > 1:
+                return True
+            else:
+                return False
+
+        # If the user chooses both cannibals to send...
+        if user_choice == 'c' and user_choice2 == "c":
+            if check_dict['cannibals'] > 1:
+                return True
+            else:
+                return False
+
+        # If the user chooses one missionary and one cannibal...
+        if user_choice == 'm' and user_choice2 == "c":
+            if check_dict['cannibals'] > 0 and check_dict['missionaries'] > 0:
+                return True
+            else:
+                return False
+
+        # Same as above, just with reversed logic choice
+        if user_choice == 'c' and user_choice2 == "m":
+            if check_dict['cannibals'] > 0 and check_dict['missionaries'] > 0:
+                return True
+            else:
+                return False
+
+        # If the user chooses one missionary or one cannibal and no...
+        if user_choice == 'm' and user_choice2 == "n":
+            if check_dict['missionaries'] > 0:
+                return True
+            else:
+                return False
+
+        # Same as above, just with reversed logic choice
+        if user_choice == 'c' and user_choice2 == "n":
+            if check_dict['cannibals'] > 0:
+                return True
+            else:
+                return False
+
+        return False
 
     def ValidateMove(self, user_choice, east_bank, west_bank, canoe, user_choice2=None):
         """ Handles the logic for validating a move """
 
-        #print("USERCHOICE = ", user_choice)
-
         if canoe['location'] == 'west':
-            if user_choice == 'm':
-                if west_bank['missionaries'] > 0:
-                    return True
-                else:
-                    return False
-            if user_choice  == 'c':
-                if west_bank['cannibals'] > 0:
-                    return True
-                else:
-                    return False
-        ##########################################
+            value = self._ValidateMove(west_bank, user_choice, user_choice2)
+            if value == True:
+                return True
+            else:
+                return False
 
-        if canoe['location'] == 'east':
-            # If the user chooses both missionaries to send...
-            if user_choice == 'm' and user_choice2 == "m":
-                if east_bank['missionaries'] > 1:
-                    return True
-                else:
-                    return False
-
-            # If the user chooses both cannibals to send...
-            if user_choice == 'c' and user_choice2 == "c":
-                if east_bank['cannibals'] > 1:
-                    return True
-                else:
-                    return False
-
-            # If the user chooses one missionary and one cannibal...
-            if user_choice == 'm' and user_choice2 == "c":
-                if east_bank['cannibals'] > 0 and east_bank['missionaries'] > 0:
-                    return True
-                else:
-                    return False
-
-            # Same as above, just with reversed logic choice
-            if user_choice == 'c' and user_choice2 == "m":
-                if east_bank['cannibals'] > 0 and east_bank['missionaries'] > 0:
-                    return True
-                else:
-                    return False
+        if canoe['location'] == "east":
+            value = self._ValidateMove(east_bank, user_choice, user_choice2)
+            if value == True:
+                return True
+            else:
+                return False
